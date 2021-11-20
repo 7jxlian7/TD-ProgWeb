c28d57c34cc5fa79548595a59fba240ee88a78fe
Julian Forme <julian.forme@etu.u-bordeaux.fr>
2021-11-20 15:02:51 +0100

    cleaning code

--- Site/inc/like.php
+++ Site/inc/like.php
@@ -5,16 +5,17 @@ session_start();
 include('db.php');
 
 if(isset($_GET['id']) && isset($_SESSION['id'])){
+    // Test d'existence de la série
     $req = $db->prepare('SELECT id FROM series WHERE id = ?');
     $req->execute([$_GET['id']]);
     $count = $req->rowCount();
     $serieId = $_GET['id'];
     if($count == 1){
+        // Est-ce que l'utilisateur a déjà liké ou pas ?
         $req = $db->prepare('SELECT * FROM user_series WHERE user_id = ? AND series_id = ?');
         $req->execute([$_SESSION['id'], $serieId]);
         $isLiked = $req->rowCount() == 0 ? false : true;
         if(!$isLiked){
-            echo $_SESSION['id'];
             $req1 = $db->prepare('INSERT INTO user_series(user_id, series_id) VALUES (?,?)');
             $req1->execute([$_SESSION['id'], $serieId]);
         } else {
